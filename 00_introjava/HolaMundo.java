9c1f8362b8f3f7ff8c77cb10ee030a174fc71bc9
Julian <juliln_11@hotmail.com>
2019-11-06 15:52:05 +0100

    -Empezando :HolaMundo.java

---
+++ 00_introjava/HolaMundo.java
@@ -0,0 +1,11 @@
+class HolaMundo{
+ public static void main(String[] argumentos) {
+	 byte unByte= 100;
+	 int unEntero = 1000;
+	 
+	 System.out.println("Tipos de datos"); 
+	 System.out.println("- Byte" + unByte);
+	 System.out.println("- Int" + unEntero);
+	// return 0;
+ }	
+}
